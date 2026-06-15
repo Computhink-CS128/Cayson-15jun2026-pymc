68adefa1e1932cf6844a2e21f2b165aa721c28e4
Computhink-CS128 <cs128@Computhink.com.sg>
2026-06-15 04:32:49 +0000

    Jun 15, 2026, 4:32 AM

--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -4,3 +4,69 @@
 
 
 ################## On Chat Commands Section #####################
+def come():
+    agent.teleport_to_player()
+
+player.on_chat("come", come)
+
+def move_fw(step):
+    agent.move(FORWARD, step)
+
+player.on_chat("fw", move_fw)
+def move_bk(step):
+    agent.move(BACK, step)
+
+player.on_chat("bk", move_bk)
+def move_up(step):
+    agent.move(UP, step)
+
+player.on_chat("up", move_up)
+def move_down(step):
+    agent.move(DOWN, step)
+
+player.on_chat("down", move_down)
+def turn_left():
+    agent.turn(TurnDirection.LEFT)
+
+player.on_chat("tl", turn_left)
+def turn_right():
+    agent.turn(TurnDirection.RIGHT)
+
+player.on_chat("tr", turn_right)
+
+def ch1():
+    agent.move(FORWARD,4)
+    agent.turn(TurnDirection.LEFT)
+    agent.move(FORWARD, 4)
+    agent.turn(TurnDirection.RIGHT)
+    agent.move(FORWARD, 3)
+    
+    for i in range(3):
+            agent.move(UP, 1)
+            agent.move(FORWARD, 1)
+    agent.move(FORWARD,1)
+    for i in range(3):
+            agent.move(FORWARD, 1)
+            agent.move(DOWN, 1)
+   
+
+player.on_chat("ch1", ch1)
+
+def ch2():
+    for i in range(3):
+        agent.move(UP, 1)
+        agent.move(FORWARD, 1)
+
+player.on_chat("ch2", ch2)
+def ch3():
+    for i in range(3):
+        agent.move(FORWARD, 1)
+        agent.move(DOWN, 1)
+
+player.on_chat("ch3", ch3)
+
+
+
+
+
+
