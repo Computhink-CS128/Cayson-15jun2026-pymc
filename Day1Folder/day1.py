f407bcbf8a07573e3afc0623b40124be2dbf4032
Computhink-CS128 <cs128@Computhink.com.sg>
2026-06-15 03:58:08 +0000

    Jun 15, 2026, 3:58 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -4,38 +4,55 @@ print("hello from day1")
 
 ########################################################################
 # Task 1:
+print("hello from day1")
 
 
 
 ########################################################################
 # Task 2:
 
-
+print("My name is Cayson")
+print("I am 11")
+print("I attend Rosyth School")
+print("I would buy a computer")
 
 ########################################################################
-# Task 3:
-
-
-
+# Task:3
+animal1 = "tiger"
+animal2 = "monkey" 
+animal3 = "elephant"
+print(animal1)
+print(animal2)
+print(animal3)
 ########################################################################
 # Task 4:
-
-
-
+num1 = 8
+num2 = 4
+print(num1 + num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 - num2)
 ########################################################################
 # Task 5:
-
-
-
+name = "Cayson"
+food = "pizza"
+print(name + " likes to eat " + food)
+print("My name is " + name)
+print("I like to eat " + food)
 ########################################################################
 # Task 6:
-
-
-
-########################################################################
-# Task 7:
+def intro():
+    print("My name is Cayson")
+    print("I am 11")
+    print("I attend Rosyth School")
+    print("I would buy a computer")
+    
+intro()
 
 
 
 ########################################################################
 # Additional exercises:
+def addition(num1,num2):
+    print(num1 + num2)
+addition(8,2)
