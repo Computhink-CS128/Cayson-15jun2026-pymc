5325a40814cd575896c26a54a1fbf64498e58d25
Computhink-CS128 <cs128@Computhink.com.sg>
2026-06-17 03:04:36 +0000

    Jun 17, 2026, 3:04 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -67,8 +67,12 @@ print("hello from day3")
 import random
 num1 = random.randint(1,1000)
 num2 = random.randint(1,1000)
-print("What is the sum of " + str(num1) +" and "+ str(num2))
-answer = input(str(num1) +" + "+ str(num2))
+
+studentAnswer = int(input("What is the sum of " + str(num1) +" and "+ str(num2)))
+answer = num1 + num2
+if answer == studentAnswer:
+    print(Correct)
+    
 
 
 ########################################################################
