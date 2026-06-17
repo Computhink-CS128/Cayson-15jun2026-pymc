478bccd01d51e756405710f4242a188376a24250
Computhink-CS128 <cs128@Computhink.com.sg>
2026-06-17 01:43:07 +0000

    Jun 17, 2026, 1:43 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -20,8 +20,8 @@ print ( name + " has " + str(num_pens) + " pens. ")
 
 ########################################################################
 # Task 3:
-num1 = input("What is your number?")
-num2 = input("What is your number?)
+int(num1 = input("What is your number?"))
+int    (num2 = input("What is your number?"))
 print(num1 + num2)
 
 
