01d41ef100dfc2055cb88536d95a577deb766ace
Computhink-CS128 <cs128@Computhink.com.sg>
2026-06-17 02:54:05 +0000

    Jun 17, 2026, 2:54 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -67,7 +67,7 @@ print("hello from day3")
 import random
 num1 = random.randint(1,1000)
 num2 = random.randint(1,1000)
-print("What is the sum of " + str(num1) +"and"+ str(num2))
+print("What is the sum of " + str(num1) +" and "+ str(num2))
 
 
 
